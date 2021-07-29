ae561bd4b02e2a2c3bffdd33a0dc45b88a915449
Pragati <phadkepragati36@gmail.com>
2021-07-29 16:16:38 +0530

    js changes

--- main.js
+++ main.js
@@ -14,12 +14,7 @@
 			BlackBerry: function() {
 			return navigator.userAgent.match(/BlackBerry/i);
 		},
-			iOS: function() {
-			return navigator.userAgent.match(/iPhone|iPad|iPod/i);
-		},
-			Opera: function() {
-			return navigator.userAgent.match(/Opera Mini/i);
-		},
+			
 			Windows: function() {
 			return navigator.userAgent.match(/IEMobile/i);
 		},
